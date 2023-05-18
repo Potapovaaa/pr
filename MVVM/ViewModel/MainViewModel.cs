e12ae4c2551708260baacf29daa0078943e90e29
Екатерина <112872136+Potapovaaa@users.noreply.github.com>
2023-05-18 12:05:45 +0300

    Add files via upload

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MVVM.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM.ViewModel
+{
+    internal class MainViewModel:BindingHelper
+    {
+        private void buttonvxod_Click(object sender, RoutedEventArgs e)
+        {
+            klava window = new klava();
+            window.Show();
+            
+        }
+    }
+}
